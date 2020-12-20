105e8e1117267981f5dfa45e3030bc2d70961ebe
Christoph Wedenig <christoph@wedenig.org>
2020-12-20 15:40:42 +0100

    v1 changes

---
+++ Assets/Scripts/CamSwitchScript.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using Cinemachine;
+using UnityEngine;
+
+public class CamSwitchScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public List<CinemachineVirtualCamera> cameras;
+    public int activeCamIndex;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            activeCamIndex = (activeCamIndex + 1) % cameras.Count;
+            for (int i = 0; i < cameras.Count; i++)
+            {
+                cameras[i].gameObject.SetActive(i == activeCamIndex);
+            }
+        }
+    }
+}
